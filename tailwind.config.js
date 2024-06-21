42deeabc49a9c275156373e26e056d75da633890
kenny516 <126701465+kenny516@users.noreply.github.com>
2024-06-21 20:02:36 +0300

    project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,php}",
+    "./pages/**/*.{html,js,php}",
+    "./components/**/*.{html,js,php}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
